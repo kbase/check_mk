3d3aa9910391a72f6d960b0914ed98cbf0ffcacd
kkellerlbl <kkeller@lbl.gov>
2018-02-15 14:27:58 -0800

    Create check_container_logs.py
    Check that the container exists and that recent logs for the container exist.

---
+++ check_container_logs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import urllib, json
+#from os import walk, listdir
+#from os.path import isdir,join
+import time
+import requests_unixsocket
+
+# list of containers to check the logs for
+containers = [ "ci_coordinator_1" , "ci_worker_1", "ci_worker_2" ]
+
+now = time.time() - (1*60)
+
+status = dict()
+
+session=requests_unixsocket.Session()
+#r=session.get('http+unix://%2fvar%2frun%2fdocker.sock/containers/ci_worker_11/logs?stderr=1&stdout=1&tail=1&since=' + str(int(now)))
+
+for name in containers:
+  status = 3
+  statusText = 'UNKNOWN'
+  checkOutput = ''
+
+# to do: need to filter coordinator diag lines
+# may need tail=larger to get enough lines
+  r=session.get('http+unix://%2fvar%2frun%2fdocker.sock/containers/' + name + '/logs?stderr=1&stdout=1&tail=1&since=' + str(int(now)) )
+
+  if r.status_code == 200:
+
+    if (len(r.content) > 10):
+      status = 0
+      statusText = 'OK'
+    else:
+      status = 2
+      statusText = 'CRITICAL'
+  else:
+      status = 3
+      statusText = 'UNKNOWN'
+
+  print str(status) + ' ' + name + '_containerlogs - ' + statusText + ' ' + r.content.strip()
