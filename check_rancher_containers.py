7049503cfcafd3ec8fa4c80b7108fdbde94e2a42
kkellerlbl <kkeller@lbl.gov>
2018-10-03 15:58:06 -0700

    Update check_rancher_containers.py

--- check_rancher_containers.py
+++ check_rancher_containers.py
@@ -3,21 +3,26 @@
 import sys
 import requests
 
+# should use argparse
 # include the port if needed
 urlbase=sys.argv[1]
+# would be better to use envname and query the api to find envid
+# but the envid should seldom if ever change so getting on cmdline ok
 envid=sys.argv[2]
-username=sys.argv[3]
-password=sys.argv[4]
+envname=sys.argv[3]
+stackname=sys.argv[4]
+username=sys.argv[5]
+password=sys.argv[6]
+
+# look for these services (remaining cmdline args)
+monitoredServices = sys.argv[7:]
 
 session=requests.Session()
-req=session.get(urlbase+'/v1/projects/' + envid + '/hosts/', auth=(username,password))
-data=req.json()['data']
+hostsReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/hosts/', auth=(username,password))
+hostData=hostsReq.json()['data']
 
-# need to print something here that makes sense if no hosts were found
-#if (len(data) == 0):
-#    print '3 no hosts UNKNOWN'
-
-for host in data:
+# monitor an agent
+for host in hostData:
 	state=3
 	stateText='UNKNOWN'
 
@@ -30,9 +35,36 @@ for host in data:
 
 	instanceReq=session.get(host['links']['instances'],auth=(username,password))
 	instanceData=instanceReq.json()['data']
-	instances=''
 #	print len(instanceData)
 #	for instance in instanceData:
-#		instances = instances + instance['name'] + ', '
-#	print instances
+#		print instance
 	print str(state) + ' rancher_agent_' + host['hostname'] + ' - ' + stateText + ' host ' + host['hostname'] + ' running containers: ' + str(len(instanceData))
+
+# to do: monitor services inside a stack
+stackReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/stacks/', auth=(username,password))
+stackData=stackReq.json()['data']
+
+# assume there's only one
+stackId = [i for i,j in enumerate(stackData) if j['name'] == stackname][0]
+for serviceId in stackData[stackId]['serviceIds']:
+#	print serviceId
+# in that stack, look through serviceIds for named services in /v2-beta/projects/envid/services/serviceId
+	serviceReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/services/' + serviceId, auth=(username,password))
+        svc=serviceReq.json()
+	if svc['name'] in monitoredServices:
+            serviceState = 3
+            serviceStateTxt = 'UNKNOWN'
+
+            if svc['healthState'] == 'healthy':
+		serviceState = 0
+                serviceStateTxt = 'OK'
+            if svc['healthState'] == 'unhealthy':
+		serviceState = 2
+                serviceStateTxt = 'CRITICAL'
+
+            print str(serviceState) + ' ' + envname + '_' + stackname + '_' + svc['name'] + ' - ' + serviceStateTxt + ' running instances: ' + str(svc['currentScale'])
+#	    print svc['healthState']
+
+
+# in each service find the last logs?  may be hard, need websocket
+
