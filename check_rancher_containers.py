181bab7063286ef72d6f6f7a3901927ad0edfe14
kkellerlbl <kkeller@lbl.gov>
2020-04-16 13:38:56 -0700

    Update check_rancher_containers.py
    python3-style print (should also work with python2)

--- check_rancher_containers.py
+++ check_rancher_containers.py
@@ -44,7 +44,7 @@ for host in hostData:
 #	print len(instanceData)
 #	for instance in instanceData:
 #		print instance
-	print str(state) + ' rancher_agent_' + host['hostname'] + ' - ' + stateText + ' host ' + host['hostname'] + ' running containers: ' + str(len(instanceData))
+	print (str(state) + ' rancher_agent_' + host['hostname'] + ' - ' + stateText + ' host ' + host['hostname'] + ' running containers: ' + str(len(instanceData)))
 
 # to do: monitor services inside a stack
 stackReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/stacks/', auth=(username,password))
@@ -73,7 +73,7 @@ for serviceId in stackData[stackId]['serviceIds']:
 		serviceState = 2
                 serviceStateTxt = 'CRITICAL'
 
-            print str(serviceState) + ' ' + envname + '_' + stackname + '_' + svc['name'] + ' - ' + serviceStateTxt + ' running instances: ' + str(svc['currentScale'])
+            print (str(serviceState) + ' ' + envname + '_' + stackname + '_' + svc['name'] + ' - ' + serviceStateTxt + ' running instances: ' + str(svc['currentScale']))
 #	    print svc['healthState']
 
 
