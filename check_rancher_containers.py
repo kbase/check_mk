b9423004013e5c55943a2457a8af0b6a803b07b8
kkellerlbl <kkeller@lbl.gov>
2020-04-16 14:37:41 -0700

    Update check_rancher_containers.py

--- check_rancher_containers.py
+++ check_rancher_containers.py
@@ -24,74 +24,74 @@ for section in args.section:
 def process_section(conf, section):
 
 # include the port if needed
-urlbase=conf[section]['rancher_url']
+	urlbase=conf[section]['rancher_url']
 # would be better to use envname and query the api to find envid
 # but the envid should seldom if ever change so getting on cmdline ok
-envid=conf[section]['rancher_envid']
-envname=conf[section]['rancher_envname']
-stackname=conf[section]['rancher_stackname']
-username=conf[section]['rancher_accesskey']
-password=conf[section]['rancher_secretkey']
+	envid=conf[section]['rancher_envid']
+	envname=conf[section]['rancher_envname']
+	stackname=conf[section]['rancher_stackname']
+	username=conf[section]['rancher_accesskey']
+	password=conf[section]['rancher_secretkey']
 
 # look for these services (a JSON-formatted list, requires double-quotes around strings)
-try:
-	monitoredServices = json.loads(conf.get(args.section,'service_list'))
-except:
-	monitoredServices = []
-#print (monitoredServices)
+	try:
+		monitoredServices = json.loads(conf.get(args.section,'service_list'))
+	except:
+		monitoredServices = []
+	#print (monitoredServices)
 
-session=requests.Session()
-hostsReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/hosts/', auth=(username,password))
-hostData=hostsReq.json()['data']
+	session=requests.Session()
+	hostsReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/hosts/', auth=(username,password))
+	hostData=hostsReq.json()['data']
 
 # monitor an agent
-for host in hostData:
-	state=3
-	stateText='UNKNOWN'
+	for host in hostData:
+		state=3
+		stateText='UNKNOWN'
 
-	if (host['state'] != 'active'):
-		state=2
-		stateText='CRITICAL'
-	if (host['state'] == 'active'):
-		state=0
-		stateText='OK'
+		if (host['state'] != 'active'):
+			state=2
+			stateText='CRITICAL'
+		if (host['state'] == 'active'):
+			state=0
+			stateText='OK'
 
-	instanceReq=session.get(host['links']['instances'],auth=(username,password))
-	instanceData=instanceReq.json()['data']
-#	print len(instanceData)
-#	for instance in instanceData:
-#		print instance
-	print (str(state) + ' rancher_agent_' + host['hostname'] + ' - ' + stateText + ' host ' + host['hostname'] + ' running containers: ' + str(len(instanceData)))
+		instanceReq=session.get(host['links']['instances'],auth=(username,password))
+		instanceData=instanceReq.json()['data']
+	#	print len(instanceData)
+	#	for instance in instanceData:
+	#		print instance
+		print (str(state) + ' rancher_agent_' + host['hostname'] + ' - ' + stateText + ' host ' + host['hostname'] + ' running containers: ' + str(len(instanceData)))
 
 # to do: monitor services inside a stack
-stackReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/stacks/', auth=(username,password))
-stackData=stackReq.json()['data']
+	stackReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/stacks/', auth=(username,password))
+	stackData=stackReq.json()['data']
 
 # assume there's only one
-try:
-	stackId = [i for i,j in enumerate(stackData) if j['name'] == stackname][0]
-except:
-	# assume no stack data; this is bad and need better handling
-	sys.exit(0)
+	try:
+		stackId = [i for i,j in enumerate(stackData) if j['name'] == stackname][0]
+	except:
+# assume no stack data; this is bad and need better handling
+		sys.exit(0)
 
-for serviceId in stackData[stackId]['serviceIds']:
-#	print serviceId
+	for serviceId in stackData[stackId]['serviceIds']:
+	#	print serviceId
 # in that stack, look through serviceIds for named services in /v2-beta/projects/envid/services/serviceId
-	serviceReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/services/' + serviceId, auth=(username,password))
-	svc=serviceReq.json()
-	if svc['name'] in monitoredServices:
-		serviceState = 3
-		serviceStateTxt = 'UNKNOWN'
+		serviceReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/services/' + serviceId, auth=(username,password))
+		svc=serviceReq.json()
+		if svc['name'] in monitoredServices:
+			serviceState = 3
+			serviceStateTxt = 'UNKNOWN'
 
-		if svc['healthState'] == 'healthy':
-			serviceState = 0
-			serviceStateTxt = 'OK'
-		if svc['healthState'] == 'unhealthy':
-			serviceState = 2
-			serviceStateTxt = 'CRITICAL'
+			if svc['healthState'] == 'healthy':
+				serviceState = 0
+				serviceStateTxt = 'OK'
+			if svc['healthState'] == 'unhealthy':
+				serviceState = 2
+				serviceStateTxt = 'CRITICAL'
 
-		print (str(serviceState) + ' ' + envname + '_' + stackname + '_' + svc['name'] + ' - ' + serviceStateTxt + ' running instances: ' + str(svc['currentScale']))
-#	    print svc['healthState']
+			print (str(serviceState) + ' ' + envname + '_' + stackname + '_' + svc['name'] + ' - ' + serviceStateTxt + ' running instances: ' + str(svc['currentScale']))
+	#	    print svc['healthState']
 
 
 # in each service find the last logs?  may be hard, need websocket
