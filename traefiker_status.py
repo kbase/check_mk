821f1c4d4434e1d2e1029d03f66cecb79d0a3aa8
kkellerlbl <kkeller@lbl.gov>
2020-04-17 14:01:40 -0700

    Update traefiker_status.py
    Able to process multiple sections of a config file

--- traefiker_status.py
+++ traefiker_status.py
@@ -1,21 +1,51 @@
 import requests
 import sys
+import configparser
+import argparse
+import json
 
-url=sys.argv[1]
-token=sys.argv[2]
+parser = argparse.ArgumentParser(description='Query Traefiker narrative_status/ and report state.')
+parser.add_argument('--config-file', dest='configfile', required=True,
+		    help='Path to config file (INI format). (required)')
+parser.add_argument('--config-sections', dest='sections', nargs='*',
+		    help='Section(s) in config file to use. (default to all sections in config file)')
+args = parser.parse_args()
 
-cookies = dict()
-cookies['kbase_session'] = token
+configfile=args.configfile
+conf=configparser.ConfigParser()
+conf.read(configfile)
+#print (conf.sections())
 
-req = requests.get(url , cookies=cookies)
+# skip to end for loop that processes each section
 
-counts = { 'active': 0, 'queued': 0 , 'total': 0}
+def process_section(conf, section):
 
-for narrative in req.json()['narrative_services']:
-    counts['total'] += 1
-    if narrative['state'] == 'active':
-        counts['active'] += 1
-    if narrative['state'] == 'queued':
-        counts['queued'] += 1
+	url=sys.argv[1]
+	token=sys.argv[2]
 
-print (counts)
+	cookies = dict()
+	cookies['kbase_session'] = token
+
+	req = requests.get(url , cookies=cookies)
+
+	counts = { 'active': 0, 'queued': 0 , 'total': 0}
+
+	for narrative in req.json()['narrative_services']:
+    	counts['total'] += 1
+		if narrative['state'] == 'active':
+			counts['active'] += 1
+		if narrative['state'] == 'queued':
+        	counts['queued'] += 1
+
+	print (counts)
+
+# main loop
+# if args provided, use them, otherwise use sections from config file
+if args.sections:
+	sections = args.sections
+else:
+	sections = conf.sections()
+
+for section in sections:
+#	print (section)
+	process_section(conf, section)
