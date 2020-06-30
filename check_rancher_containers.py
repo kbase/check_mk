53bafa1d4db6ef028af1ce32948853b62c112b7a
kkellerlbl <kkeller@lbl.gov>
2020-06-30 13:43:42 -0700

    Test printing memory use of containers

--- check_rancher_containers.py
+++ check_rancher_containers.py
@@ -105,17 +105,16 @@ def process_section(conf, section):
 # if on a host running containers, check their resources
 # assume only one instance
 # broken?
-#		instanceReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/instances/' + svc['instanceIds'][0], auth=(username,password))
-#		rancherInstance=instanceReq.json()
-#		if rancherInstance['hostId'] == hostid:
-#			print (rancherInstance['name'])
-#			dockerClient = docker.from_env()
-#			dockerContainer = dockerClient.containers.get(rancherInstance['externalId'])
+		instanceReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/instances/' + svc['instanceIds'][0], auth=(username,password))
+		rancherInstance=instanceReq.json()
+		if rancherInstance['hostId'] == hostid:
+			print (rancherInstance['name'])
+			dockerClient = docker.from_env()
+			dockerContainer = dockerClient.containers.get(rancherInstance['externalId'])
 # need to put this into check_mk format (and make only one line of output for all containers)
-#			print (svc['name'] + ' ' + str(dockerContainer.stats(stream=False)['memory_stats']['usage']))
+			print (svc['name'] + ' ' + str(dockerContainer.stats(stream=False)['memory_stats']['usage']))
 
-#		rancherHostReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/services/' + serviceId, auth=(username,password))
-#		if os.uname()[1] == 
+		rancherHostReq=session.get(urlbase+'/v2-beta/projects/' + envid + '/services/' + serviceId, auth=(username,password))
 
 
 # in each service find the last logs?  may be hard, need websocket
