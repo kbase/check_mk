d6730aade0cc64eec25dc0e8b9cb90bf45b4fd3e
kkellerlbl <kkeller@lbl.gov>
2021-08-17 11:14:53 -0700

    Update check_num_fds.py

--- check_num_fds.py
+++ check_num_fds.py
@@ -48,6 +48,44 @@ import sys
 if options.verbose : print ("Checking if the provided string: " + options.file + " is really a file.")
 assert os.path.isfile(options.file)
 
+def check_pid(pid):
+  if options.proc :
+    try:
+      if options.verbose : print ("Opening the file: /proc/"+str( pid )+"/limits")
+      procfile = open('/proc/'+str( pid )+'/limits','r')
+      for line in procfile:
+        if options.verbose : print ("Searching for the 'Max open files' settings in: " + line)
+        if "Max open files" in line : 
+             mylist = [ int(s) for s in line.split() if s.isdigit()]
+             options.warn_value = mylist[0]
+             options.crit_value = mylist[1]
+             if options.verbose : print ( "Found soft limit: " + str( options.warn_value ) )
+             if options.verbose : print ( "Found hard limit: " + str( options.crit_value ) )
+             break
+
+    except IOError:
+      print ( "Can't open the file %s", options.file)
+      sys.exit(1)
+
+
+# Getting the number of files opened by pid
+  num_fds = psutil.Process( pid ).num_fds()
+
+  assert options.warn_value > 0
+  assert options.crit_value > 0
+
+# Nagios possible states
+  status_dict= {0:"OK",1:"WARNING",2:"CRITICAL",3:"UNKNOWN"}
+
+  if num_fds > options.crit_value:
+      status=2
+  elif num_fds > options.warn_value:
+      status=1
+  else:
+      status=0
+
+  print ("{0}: Process {1} has {2} file descriptors opened|num_fds={2};{3};{4};;".format(status_dict[status], str( pid ), str( num_fds ), str(options.warn_value), str(options.crit_value) ) )
+
 try:
   if options.verbose : print ("Opening file: " + options.file)
   pidfile = open(options.file,'r')
@@ -59,41 +97,6 @@ except IOError:
   print ("Can't open the file %s", options.file)
   sys.exit(1)   
 
-if options.proc :
-  try:
-    if options.verbose : print ("Opening the file: /proc/"+str( pid )+"/limits")
-    procfile = open('/proc/'+str( pid )+'/limits','r')
-    for line in procfile:
-      if options.verbose : print ("Searching for the 'Max open files' settings in: " + line)
-      if "Max open files" in line : 
-           mylist = [ int(s) for s in line.split() if s.isdigit()]
-           options.warn_value = mylist[0]
-           options.crit_value = mylist[1]
-           if options.verbose : print ( "Found soft limit: " + str( options.warn_value ) )
-           if options.verbose : print ( "Found hard limit: " + str( options.crit_value ) )
-           break
+check_pid(pid)
 
-  except IOError:
-    print ( "Can't open the file %s", options.file)
-    sys.exit(1)
-
-
-# Getting the number of files opened by pid
-num_fds = psutil.Process( pid ).num_fds()
-
-
-assert options.warn_value > 0
-assert options.crit_value > 0
-
-# Nagios possible states
-status_dict= {0:"OK",1:"WARNING",2:"CRITICAL",3:"UNKNOWN"}
-
-if num_fds > options.crit_value:
-    status=2
-elif num_fds > options.warn_value:
-    status=1
-else:
-    status=0
-
-print ("{0}: Process {1} has {2} file descriptors opened|num_fds={2};{3};{4};;".format(status_dict[status], str( pid ), str( num_fds ), str(options.warn_value), str(options.crit_value) ) )
 exit(status)
