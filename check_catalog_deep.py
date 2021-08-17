05056dcfbc31337e143134968ad685d6a54a2e45
bio-boris <bio-boris@users.noreply.github.com>
2021-08-17 15:49:27 -0500

    Update check_catalog_deep.py

--- check_catalog_deep.py
+++ check_catalog_deep.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python
 # This is used to do a deep catalog ping as per #DEVOPS-459
-# This excercises endpoints that require a connection to mongo, rather than just the status endpoint
+# These checks excercises endpoints that require a connection to MongoDB, (rather than just the status endpoint)
 
-import sys
 import requests
-import json
 
 import urllib3
+
 # there is a way to only disable InsecurePlatformWarning but I can't find it now
 urllib3.disable_warnings()
 
-catalog_url = https://kbase.us/services/catalog
-services = sys.argv[2:]
-
-
+catalog_url = 'https://kbase.us/services/catalog'
 
 # curl -d '{"params":["bsadkhin"],"method":"Catalog.list_favorites","version":"1.1","id":1}'  https://kbase.us/services/catalog
-list_favorites_query =  {
-        "id": 47,
-        "version": "1.1",
-        "method": "Catalog.list_favorites",
-        "params": [{"params":["bsadkhin"]}]
-    }
-
+list_favorites_query = {
+    "id": 47,
+    "version": "1.1",
+    "method": "Catalog.list_favorites",
+    "params": ["bsadkhin"]
+}
 # curl -d '{"params":[{}],"method":"Catalog.list_favorite_counts","version":"1.1","id":1}'  https://kbase.us/services/catalog
-list_favorite_counts =  {
-        "id": 47,
-        "version": "1.1",
-        "method": "Catalog.list_favorite_counts",
-        "params": [{"params":[{}]}]
-    }
+list_favorite_counts = {
+    "id": 47,
+    "version": "1.1",
+    "method": "Catalog.list_favorite_counts",
+    "params": [{}]
+}
+
+catalog_ok = True
 
 for query in [list_favorites_query, list_favorite_counts]:
-    query=requests.get(sw_url, json=swdata)
+    result = requests.get(catalog_url, json=query)
 
     try:
-        dynsvcurl = swreq.json()['result'][0]['url']
-        print '0 - OK - service ' + dynsvc + ' not responding correctly'
-    except:
-        print '2 - CRITICAL - catalog deep ping failed '
-        continue
+        result_json = result.json()['result'][0]
+        assert len(result_json) > 1
+    except Exception as e:
+        catalog_ok = False
 
+if catalog_ok:
+    print('0 - OK - Catalog Deep Ping ')
+else:
+    print('2 - CRITICAL - catalog deep ping failed ')
