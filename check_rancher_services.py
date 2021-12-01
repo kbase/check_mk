32a74f24e5d6181a355c0380755b05b747770f29
kkellerlbl <kkeller@lbl.gov>
2021-12-01 11:05:01 -0800

    Update check_rancher_services.py

--- check_rancher_services.py
+++ check_rancher_services.py
@@ -157,8 +157,7 @@ def process_section(conf, section):
 			if (conf.has_option(section,'stack_health_dir') and stackPath.exists()):
 			    # check age, if too old, make state critical
 			    # if missing, don't do anything?
-			    pprint (stackPath.stat().st_mtime)
-			    pprint (time.time())
+			    pprint (stackPath.stat().st_mtime - time.time())
 
 		print (str(stackState) + ' ' + envname + '_' + stackname + '_stackHealth - ' + stackStateTxt + ' stack health is ' + stackData[myStack]['healthState'])
 
