a62c5abdd06769eea7dc24f3c3c8c74e496d106e
bio-boris <bio-boris@users.noreply.github.com>
2023-12-14 15:47:53 -0600

    Create check_htcondor_workers.py

---
+++ check_htcondor_workers.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import htcondor
+
+def get_worker_status():
+    coll = htcondor.Collector()
+    ads = coll.query(htcondor.AdTypes.Startd, projection=["Name", "NodeIsHealthy"])
+
+    healthy_workers = []
+    unhealthy_workers = []
+
+    for ad in ads:
+        name = ad.get('Name', 'Unknown')
+        is_healthy = ad.get('NodeIsHealthy', False)
+
+        if is_healthy:
+            healthy_workers.append(name)
+        else:
+            unhealthy_workers.append(name)
+
+    return healthy_workers, unhealthy_workers
+
+def main():
+    healthy_workers, unhealthy_workers = get_worker_status()
+
+    print("Healthy Workers:")
+    for worker in healthy_workers:
+        print(worker)
+
+    print("\nUnhealthy Workers:")
+    for worker in unhealthy_workers:
+        print(worker)
+
+    # Determine if the situation is critical
+    if unhealthy_workers:
+        print("\nCRITICAL: There are unhealthy workers!")
+        # Insert your Checkmk alerting mechanism here
+        # Example: alert_checkmk_critical(unhealthy_workers)
+    else:
+        print("\nOK: All htcondor workers are healthy.")
+
+if __name__ == "__main__":
+    main()
