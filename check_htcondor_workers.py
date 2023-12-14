b956270cc945d6d29a55cc0e14b4ed2d72090b64
bio-boris <bio-boris@users.noreply.github.com>
2023-12-14 15:50:58 -0600

    Update check_htcondor_workers.py

--- check_htcondor_workers.py
+++ check_htcondor_workers.py
@@ -4,14 +4,14 @@ import htcondor
 
 def get_worker_status():
     coll = htcondor.Collector()
-    ads = coll.query(htcondor.AdTypes.Startd, projection=["Name", "NodeIsHealthy"])
+    ads = coll.query(htcondor.AdTypes.Startd, projection=["Name", "node_is_healthy"])
 
     healthy_workers = []
     unhealthy_workers = []
 
     for ad in ads:
         name = ad.get('Name', 'Unknown')
-        is_healthy = ad.get('NodeIsHealthy', False)
+        is_healthy = ad.get('node_is_healthy', False)
 
         if is_healthy:
             healthy_workers.append(name)
@@ -23,20 +23,11 @@ def get_worker_status():
 def main():
     healthy_workers, unhealthy_workers = get_worker_status()
 
-    print("Healthy Workers:")
-    for worker in healthy_workers:
-        print(worker)
-
-    print("\nUnhealthy Workers:")
-    for worker in unhealthy_workers:
-        print(worker)
-
-    # Determine if the situation is critical
     if unhealthy_workers:
-        print("\nCRITICAL: There are unhealthy workers!")
+        print(f"CRITICAL: There are unhealthy workers {unhealthy_workers}")
 
     else:
-        print("\nOK: All htcondor workers are healthy.")
+        print("OK: All htcondor workers are healthy.")
 
 if __name__ == "__main__":
     main()
